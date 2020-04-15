c5372d1db8cef793e088173a2f34dbbd05d879e4
Alexander <ezyyk77@gmail.com>
2020-04-15 14:26:49 +0300

    tilde fixed

--- src/input/dollar_exp.c
+++ src/input/dollar_exp.c
@@ -1,19 +1,69 @@
 #include "ush.h"
 
+// static char *get_key(char *str) {
+//     char *variable = NULL;
+
+//     if (strlen(str) > 1 && str[1] == '{') {
+//         for (int i = 2; i < strlen(str); i++) {
+//             if (str[i] == '}') {
+//                 variable = strndup(str + 2, i);
+//             }
+//             else if (!isalpha(str[i]) && !isnumber(str[i]))
+                
+//         }
+//     }
+//     else {
+//         for (int i = 0; i < strlen(str); i++) {
+//             if (!isalpha(str[i]) && !isnumber(str[i])) {
+//                 variable = strndup(str, i);
+//                 break;
+//             }
+//         }
+//     }
+//     return variable;
+// }
+
+// static void clear_undefined_variable(t_ush *ush, size_t index) {
+
+// }
+
+// void mx_expand_dollar(t_ush *ush, size_t index) {
+//     char *key = NULL;
+//     char *value = NULL;
+//     char *line = ush->in->line;
+
+//     if (index != 0 && !isspace(line[index - 1]))
+//         return;
+//     key = get_key(line + index);
+//     if (key) {
+//         value = strcmp(key, "?") == 0 ? mx_getenv(ush->hidden, key)
+//             : mx_getenv(ush->local_variables, key);
+//     }
+//     else {
+//         clear_undefined_variable(ush, index);
+//     }
+// }
+
+
 void mx_expand_dollar(t_ush *ush, size_t index, char *postfix) {
     char *key = NULL;
     char *value = NULL;
     char *line = ush->in->line;
     char *tmp = mx_strdup(line + index + 1);
 
-    for (size_t i = index; i < strlen(postfix); i++)
-        if (isspace(postfix[i]))
+    for (size_t i = 0; i < strlen(postfix); i++)
+        if (isspace(postfix[i]) || postfix[i] == ')' || postfix[i] == '\\'
+            || postfix[i] == '`') {
             key = strndup(postfix, i);
-    key = key ? key : strndup(postfix, strlen(line));
-    value = mx_getenv(ush->hidden, key);
+            break;
+        }
+    key = key ? key : strndup(postfix, strlen(postfix));
+    value = strcmp(key, "?") == 0 ? mx_getenv(ush->hidden, key)
+        : mx_getenv(ush->local_variables, key);
     if (value) {
         if (strlen(value) + strlen(line) >= malloc_size(line))
             ush->in->line = realloc(line, malloc_size(line) + strlen(value));
+        memset(line + index, 0, strlen(line + index));
         memmove(line + index, value, strlen(value));
         memmove(line + index + strlen(value),
                 tmp + strlen(key), strlen(tmp) - strlen(key));
--- src/input/tilde_exp.c
+++ src/input/tilde_exp.c
@@ -1,42 +1,42 @@
 #include "ush.h"
 
-static bool check_path(char *line, size_t start_index) {
+static bool check_path(char *postfix) {
     char *user_path = NULL;
     char *path = NULL;
     struct stat pstat;
-    size_t end_index = strlen(line);
+    bool result;
 
-    for (size_t i = start_index; i < strlen(line); i++)
-        if (isspace(line[i])) {
-            if (i - 1 > 0 && line[i - 1] == '\\')
+    for (size_t i = 0; i < strlen(postfix); i++)
+        if (isspace(postfix[i]) || postfix[i] == ')' || postfix[i] == '`') {
+            if (i - 1 > 0 && postfix[i - 1] == '\\')
                 continue;
-            end_index = i;
+            user_path = strndup(postfix, i);
+            break;
         }
-    user_path = strndup(line + start_index + 1, end_index);
+    user_path = user_path ? user_path : strndup(postfix, strlen(postfix));
     path = mx_strjoin("/Users/", user_path);
     mx_strdel(&user_path);
-    if (stat(path, &pstat) < 0)
-        mx_strdel(&path);
-    return path;
+    result = stat(path, &pstat) < 0 ? false : true;
+    mx_strdel(&path);
+    return result;
 }
 
-static char *replace_tilde(t_ush *ush, size_t index, char *postfix) {
+static char *replace_tilde(t_ush *ush, char *postfix) {
     char *tilde = NULL;
 
-    if (postfix[0] == '+') {
+    if (postfix[0] == '+' || postfix[0] == '-') {
         if (strlen(postfix) > 1 && !isspace(postfix[1]))
-            return NULL;
-        tilde = mx_getenv(ush->hidden, "PWD");
+            if (!(postfix[1] == '`' || postfix[1] == ')'))
+                return NULL;
+        tilde = postfix[0] == '+' ? mx_getenv(ush->hidden, "PWD")
+            : mx_getenv(ush->hidden, "OLDPWD");
     }
-    else if (postfix[0] == '-') {
-        if (strlen(postfix) > 1 && !isspace(postfix[1]))
-            return NULL;
-        tilde = mx_getenv(ush->hidden, "OLDPWD");
-    }
-    else if (!postfix[0] || isspace(postfix[0]) || postfix[0] == '/')
-        tilde = mx_getenv(ush->hidden, "HOME");
+    else if (!postfix[0] || isspace(postfix[0]) || postfix[0] == '/'
+        || postfix[0] == '`' || postfix[0] == ')') {
+            tilde = mx_getenv(ush->hidden, "HOME");
+        }
     else
-        tilde = check_path(ush->in->line, index) ? strdup("/Users/") : NULL;
+        tilde = check_path(postfix) ? strdup("/Users/") : NULL;
     return tilde;
 }
 
@@ -49,13 +49,13 @@ void mx_expand_tilde(t_ush *ush, size_t index, char *postfix) {
         mx_strdel(&tmp);
         return;
     }
-    tilde = replace_tilde(ush, index, postfix);
+    tilde = replace_tilde(ush, postfix);
     if (tilde) {
         if (strlen(tilde) + strlen(line) >= malloc_size(line))
             ush->in->line = realloc(line, malloc_size(line) + strlen(tilde));
         memmove(line + index, tilde, strlen(tilde));
-        memmove(line + index + strlen(tilde)
-                + (tmp[0] == '+' || tmp[0] == '-'), tmp, strlen(tmp));
+        memmove(line + index + strlen(tilde),
+                tmp + (tmp[0] == '+' || tmp[0] == '-'), strlen(tmp));
         mx_strdel(&tilde);
     }
     mx_strdel(&tmp);
